2e6855cd88a0cb6db3acd42bfc0a7c58cc1837c9
Sqqal <Sqqal@192.168.1.92>
2021-09-14 18:41:22 +0200

    using ModelAndView method instead of httpSession

---
+++ SpringBootClientDataBrowserMVC/src/main/java/com/essgineer/demo/SpringBootClientDataBrowserMvcApplication.java
@@ -0,0 +1,13 @@
+package com.essgineer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientDataBrowserMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientDataBrowserMvcApplication.class, args);
+	}
+
+}
---
+++ SpringBootClientDataBrowserMVC/src/test/java/com/essgineer/demo/SpringBootClientDataBrowserMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essgineer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClientDataBrowserMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
